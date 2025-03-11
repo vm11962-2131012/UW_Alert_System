f2e2a092f4bfec4529deae45673bc32df371e4e8
lizethvl <lizethvl@uw.edu>
2025-03-11 16:52:49 -0700

    Edit navbar, navbar button, and about page

---
+++ js/about.js
@@ -0,0 +1,12 @@
+let icon = document.getElementsByClassName("icon")[0];
+
+icon.addEventListener('click', responsive_control);
+
+function responsive_control() {
+  let x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
--- js/main.js
+++ js/main.js
@@ -584,3 +584,16 @@ function toggleFilter(button, visibility) {
     icon.classList.add('fa-toggle-on');
   }
 }
+
+let icon = document.getElementsByClassName("icon")[0];
+
+icon.addEventListener('click', responsive_control);
+
+function responsive_control() {
+  let x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
